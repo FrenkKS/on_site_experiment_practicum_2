b060207fc8bbef0dd37293fb84663448d71ec298
FrenkKS <frenkks@gmail.com>
2020-12-10 13:01:11 +0100

    volledige analyse 1

--- algemene_analyse.py
+++ algemene_analyse.py
@@ -1,12 +1,10 @@
-## LET OP: foutenanalyse onvolledig
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from lmfit import models
 import lmfit
 
-spectrum_vacuum = pd.read_csv('ijkmetingen/ijkmeting_3_12.csv')
+spectrum_vacuum = pd.read_csv('ijkmetingen/ijkmeting_19_11_2.csv')
 
 pulseheights_ijk = spectrum_vacuum['pulseheight'].tolist()
 counts_ijk = spectrum_vacuum['counts'].tolist()
@@ -42,7 +40,77 @@ fit = model.fit(df['Pulseheight'], x=df['Energy'], weights=1/df['PH_err'], a=40)
 
 pulse_per_mev = fit.params['a'].value
 
-## Vanaf hier kopiëren in algemene analyse
+max_energies = []
+max_energies_err = []
+distances = []
+distances_err = []
+average_pressures = []
+average_pressures_err = []
+
+atm = 1013.249977
+
+pressures = [50, 100, 120, 150, 200, 300, 450, 470, 490, 500]
+pressures_err = [2, 3, 5, 5, 5, 10, 15, 15, 15, 15]
+
+pressures_2 = [60.3, 111.2, 131.4, 166.9, 224.8, 352.3, 499.1, 519.0, 536.6, 554.2]
+pressures_2_err = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+
+for index in range(len(pressures)):
+    spectrum = pd.read_csv(f'data_lucht/{pressures[index]}mbar_meting_lucht.csv')
+
+    pulseheights = spectrum['pulseheight'].tolist()
+    counts = spectrum['counts'].tolist()
+
+    max_index = np.argmax(counts)
+    max_pulseheight = pulseheights[max_index]
+    max_pulseheight_err = max_pulseheight - pulseheights[max_index - 1]
+
+    max_energy = max_pulseheight / pulse_per_mev
+    max_energy_err = max_pulseheight_err / pulse_per_mev
+    max_energies.append(max_energy)
+    max_energies_err.append(max_energy_err)
+
+    average_pressure = (pressures[index] + pressures_2[index]) / 2
+    average_pressure_err = 0.5 * np.sqrt((pressures_err[index])**2 + (pressures_2_err[index])**2)
+    average_pressures.append(average_pressure)
+    average_pressures_err.append(average_pressure_err)
+
+    factor = atm / average_pressure
+    factor_err = (atm / (average_pressure)**2) * average_pressure_err
+
+    distance = 5 / factor
+    distance_err = np.sqrt((0.05 / factor)**2 + ((5 / factor**2) * factor_err)**2)
+    distances.append(distance)
+    distances_err.append(distance_err)
+
+# plt.scatter(distances, max_energies)
+# plt.xlabel("distance (cm)")
+# plt.ylabel("max remaining energy (MeV)")
+# plt.show()
+
+measurements_air = {
+    'Max Energy': max_energies,
+    'Max Energy Error': max_energies_err,
+    'Distance': distances
+}
+
+df_air = pd.DataFrame(measurements_air)
+
+linear = lambda x, a, b: a*x + b
+model2 = models.Model(linear, name="linear")
+
+fit_air = model2.fit(df_air['Max Energy'], x=df_air['Distance'], weights=1/df_air['Max Energy Error'], a=-1, b=5.5)
+fit_air.plot()
+plt.show()
+
+stopping_power_air = fit_air.params['a'].value
+stopping_power_air_kev = stopping_power_air * -1000
+
+stopping_power_air_err = fit_air.params['a'].stderr
+stopping_power_air_kev_err = stopping_power_air_err * 1000
+
+print(f'The stopping power of air is {stopping_power_air_kev:.3f} KeV +/- {stopping_power_air_kev_err:.3f} KeV')
+
 
 max_energies_helium = []
 max_energies_helium_err = []
@@ -52,8 +120,6 @@ average_pressures_15cm = []
 average_pressures_15cm_err = []
 average_pressures_20cm = []
 average_pressures_20cm_err = []
-
-atm = 1013.249977
 
 pressures_15cm = [30, 50, 100, 120, 150, 200, 300, 450, 600, 800, 820, 840, 860]
 pressures_15cm_err = [2, 3, 5, 5, 10, 10, 15, 15, 20, 20, 20, 20, 20]
@@ -128,16 +194,13 @@ for index in range(len(pressures_20cm)):
 # plt.ylabel("max remaining energy (MeV)")
 # plt.show()
 
-measurements = {
+measurements_helium = {
     'Max Energy': max_energies_helium,
     'Max Energy Error': max_energies_helium_err,
     'Distance': distances_helium
 }
 
-df_helium = pd.DataFrame(measurements)
-
-linear = lambda x, a, b: a*x + b
-model2 = models.Model(linear, name="linear")
+df_helium = pd.DataFrame(measurements_helium)
 
 fit_helium = model2.fit(df_helium['Max Energy'], x=df_helium['Distance'], weights=1/df_helium['Max Energy Error'], a=-1, b=5.5)
 fit_helium.plot()
@@ -153,4 +216,13 @@ stopping_power_helium_kev_err = stopping_power_helium_err * 1000
 
 print(f'The stopping power of helium is {stopping_power_helium_kev:.3f} KeV +/- {stopping_power_helium_kev_err:.3f} KeV')
 
+stopping_powers_factor = stopping_power_air / stopping_power_helium
 
+theoretical_stopping_powers_factor = 1142.5 / 163.85
+
+print(f'The calculated stopping power ratio air:helium is {stopping_powers_factor:.2f}')
+print(f'The theoretical stopping power ratio air:helium is {theoretical_stopping_powers_factor:.2f}')
+
+
+c = 1 - (0.32)**(1/10)
+print(c)
--- analyse_lucht.py
+++ analyse_lucht.py
@@ -43,14 +43,19 @@ fit = model.fit(df['Pulseheight'], x=df['Energy'], weights=1/df['PH_err'], a=40)
 pulse_per_mev = fit.params['a'].value
 
 max_energies = []
+max_energies_err = []
 distances = []
+distances_err = []
+average_pressures = []
+average_pressures_err = []
 
 atm = 1013.249977
 
 pressures = [50, 100, 120, 150, 200, 300, 450, 470, 490, 500]
-pressures_2 = [60.3, 111.2, 131.4, 166.9, 224.8, 352.3, 499.1, 519.0, 536.6, 554.2]
+pressures_err = [2, 3, 5, 5, 5, 10, 15, 15, 15, 15]
 
-average_pressures = []
+pressures_2 = [60.3, 111.2, 131.4, 166.9, 224.8, 352.3, 499.1, 519.0, 536.6, 554.2]
+pressures_2_err = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 
 for index in range(len(pressures)):
     spectrum = pd.read_csv(f'data_lucht/{pressures[index]}mbar_meting_lucht.csv')
@@ -60,21 +65,51 @@ for index in range(len(pressures)):
 
     max_index = np.argmax(counts)
     max_pulseheight = pulseheights[max_index]
+    max_pulseheight_err = max_pulseheight - pulseheights[max_index - 1]
+
     max_energy = max_pulseheight / pulse_per_mev
+    max_energy_err = max_pulseheight_err / pulse_per_mev
     max_energies.append(max_energy)
+    max_energies_err.append(max_energy_err)
 
     average_pressure = (pressures[index] + pressures_2[index]) / 2
+    average_pressure_err = 0.5 * np.sqrt((pressures_err[index])**2 + (pressures_2_err[index])**2)
     average_pressures.append(average_pressure)
+    average_pressures_err.append(average_pressure_err)
 
     factor = atm / average_pressure
+    factor_err = (atm / (average_pressure)**2) * average_pressure_err
 
     distance = 5 / factor
+    distance_err = np.sqrt((0.05 / factor)**2 + ((5 / factor**2) * factor_err)**2)
     distances.append(distance)
+    distances_err.append(distance_err)
 
-plt.scatter(distances, max_energies)
-plt.xlabel("distance (cm)")
-plt.ylabel("max remaining energy (MeV)")
+# plt.scatter(distances, max_energies)
+# plt.xlabel("distance (cm)")
+# plt.ylabel("max remaining energy (MeV)")
+# plt.show()
+
+measurements = {
+    'Max Energy': max_energies,
+    'Max Energy Error': max_energies_err,
+    'Distance': distances
+}
+
+df_2 = pd.DataFrame(measurements)
+
+linear = lambda x, a, b: a*x + b
+model2 = models.Model(linear, name="linear")
+
+fit_2 = model2.fit(df_2['Max Energy'], x=df_2['Distance'], weights=1/df_2['Max Energy Error'], a=-1, b=5.5)
+fit_2.plot()
 plt.show()
 
-print(average_pressures)
+stopping_power_air = fit_2.params['a'].value
+stopping_power_air_kev = stopping_power_air * -1000
+
+stopping_power_air_err = fit_2.params['a'].stderr
+stopping_power_air_kev_err = stopping_power_air_err * 1000
+
+print(f'The stopping power of helium is {stopping_power_air_kev:.3f} KeV +/- {stopping_power_air_kev_err:.3f} KeV')
 
